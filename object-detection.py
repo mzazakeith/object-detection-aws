d8888a5e6b4a08290a61ce149bfb0413a53f898e
mzazakeith <mzazakeith@gmail.com>
2024-03-19 21:43:35 +0300

    draw lines, points fix

--- object-detection.py
+++ object-detection.py
@@ -1,6 +1,7 @@
 import csv
 import boto3
 from PIL import Image, ImageDraw, ImageFont
+import io
 
 with open('credentials.csv', 'r') as credential_file:
     next(credential_file)
@@ -12,7 +13,6 @@ with open('credentials.csv', 'r') as credential_file:
 client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=access_key_id,
                       aws_secret_access_key=secret_access_key)
 
-
 photo = 'images/person.jpg'
 
 with open(photo, 'rb') as image:
@@ -20,14 +20,29 @@ with open(photo, 'rb') as image:
 
 detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
 
+result_image = Image.open(io.BytesIO(source_bytes))
+draw = ImageDraw.Draw(result_image)
+
 for label in detect_objects['Labels']:
     print(label['Name'])
     print('Confidence:', label['Confidence'])
 
-    for(instances) in label['Instances']:
+    for (instances) in label['Instances']:
         if 'BoundingBox' in instances:
             box = instances['BoundingBox']
-            left = image.width * box['Left']
-            top = image.height * box['Top']
-            width = image.width * box['Width']
-            height = image.height * box['Height']
+            left = result_image.width * box['Left']
+            top = result_image.height * box['Top']
+            width = result_image.width * box['Width']
+            height = result_image.height * box['Height']
+
+            points = (
+                (left, top),
+                (left + width, top),
+                (left + width, top + height),
+                (left, top + height),
+                (left, top)
+            )
+
+            draw.line(points, width=5, fill="#03fccf")
+
+            result_image.show()
