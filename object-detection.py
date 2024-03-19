e422fb80dc21010c2c4da16b4c39b6e8181d2d2e
mzazakeith <mzazakeith@gmail.com>
2024-03-19 21:09:13 +0300

    boto3 client

--- object-detection.py
+++ object-detection.py
@@ -7,3 +7,6 @@ with open('credentials.csv', 'r') as credential_file:
     for line in reader:
         access_key_id = line[2]
         secret_access_key = line[3]
+
+client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=access_key_id,
+                      aws_secret_access_key=secret_access_key)
