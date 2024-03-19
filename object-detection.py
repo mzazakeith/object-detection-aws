8f9b1e56ffcff009b51f01e9baf4468b062b8ba3
mzazakeith <mzazakeith@gmail.com>
2024-03-19 21:23:52 +0300

    fix for credentials files and initial function

--- object-detection.py
+++ object-detection.py
@@ -5,8 +5,18 @@ with open('credentials.csv', 'r') as credential_file:
     next(credential_file)
     reader = csv.reader(credential_file)
     for line in reader:
-        access_key_id = line[2]
-        secret_access_key = line[3]
+        access_key_id = line[0]
+        secret_access_key = line[1]
 
 client = boto3.client('rekognition', region_name='us-east-1', aws_access_key_id=access_key_id,
                       aws_secret_access_key=secret_access_key)
+
+
+photo = 'images/person.jpg'
+
+with open(photo, 'rb') as image:
+    source_bytes = image.read()
+
+detect_objects = client.detect_labels(Image={'Bytes': source_bytes})
+
+print(detect_objects)
